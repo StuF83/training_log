d7bae48273f80721f944abd0dc509d35d0f7f765
Stuart <stuart.s.frew@gmail.com>
2024-07-12 12:34:14 +0100

    RSpec added

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,13 @@
 class Workout < ApplicationRecord
+  validates :workout_type, presence: true
+  validates :duration, presence: true, numericality: { greater_than: 0 } 
+  validates :intensity, presence: true, inclusion: { in: %w(low moderate high), message: "#{value} is not a valid intensity" }
+  validates :date, presence: true
+  validate :date_cannot_be_in_the_future
+
+  def date_cannot_be_in_the_future
+    if date.present? && date > Date.today
+      errors.add(:date, "Workout date cannot be in the future")
+    end
+  end
 end
