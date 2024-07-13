e31402a8310a93fedf982b5a35b734a3d50f19db
Stuart <stuart.s.frew@gmail.com>
2024-07-13 19:26:45 +0100

    new workout and workout show page added

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,7 +1,9 @@
 class Workout < ApplicationRecord
+INTENSITY = %w[low moderate high]
+
   validates :workout_type, presence: true
   validates :duration, presence: true, numericality: { greater_than: 0 } 
-  validates :intensity, presence: true, inclusion: { in: %w[low moderate high], message: "%{value} is not a valid intensity" }
+  validates :intensity, presence: true, inclusion: { in: INTENSITY, message: "%{value} is not a valid intensity" }
   validates :date, presence: true
   validate :date_cannot_be_in_the_future
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/workouts", to: "workouts#index"
+  resources :workouts
   
 end
