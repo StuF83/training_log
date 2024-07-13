9363ef28ca42f1ec593a7484f9aff4454912ecc9
Stuart <stuart.s.frew@gmail.com>
2024-07-13 07:34:06 +0100

    workout tests added and refactoring

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,7 +1,7 @@
 class Workout < ApplicationRecord
   validates :workout_type, presence: true
   validates :duration, presence: true, numericality: { greater_than: 0 } 
-  validates :intensity, presence: true, inclusion: { in: %w(low moderate high), message: "%{value} is not a valid intensity" }
+  validates :intensity, presence: true, inclusion: { in: %w[low moderate high], message: "%{value} is not a valid intensity" }
   validates :date, presence: true
   validate :date_cannot_be_in_the_future
 
