03cbe55dd61d3c1ea49630a36ae474263e3023b0
Stuart <stuart.s.frew@gmail.com>
2024-07-15 20:46:32 +0100

    DailyOverview validation tests implimented

--- spec/models/daily_overview_spec.rb
+++ spec/models/daily_overview_spec.rb
@@ -3,11 +3,20 @@ require 'rails_helper'
 RSpec.describe DailyOverview, type: :model do
   # Setup valid attribues to use for tests
   let(:valid_attributes) {
-    {date: Date.today}
+      {date: Date.today}
   }
+  let(:duplicate_attributes) {
+    {date: Date.today}
+}
 
     # Check that the factory is valid
     it 'is valid with valid attributes' do
       expect(DailyOverview.new(valid_attributes)).to be_valid
+    end
+
+    # Check for uniqueness validation
+    it 'is invalid if DailyOverview with same date exists' do
+      DailyOverview.create!(valid_attributes)
+      expect(DailyOverview.new(duplicate_attributes)).to_not be_valid
     end
 end
